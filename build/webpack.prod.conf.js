d83bd9d5db06dd776711ace8619fc34422f8423f
徐其闯 <qichuan.xu@ly.com>
2019-11-05 13:40:39 +0800

    排除另一个入口文件

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -72,7 +72,8 @@ const webpackConfig = merge(baseWebpackConfig, {
         // https://github.com/kangax/html-minifier#options-quick-reference
       },
       // necessary to consistently work with multiple chunks via CommonsChunkPlugin
-      chunksSortMode: 'dependency'
+      chunksSortMode: 'dependency',
+      excludeChunks: ['exhibition']
     }),
     new HtmlWebpackPlugin({
       filename: config.build.exhibition,
@@ -86,7 +87,8 @@ const webpackConfig = merge(baseWebpackConfig, {
         // https://github.com/kangax/html-minifier#options-quick-reference
       },
       // necessary to consistently work with multiple chunks via CommonsChunkPlugin
-      chunksSortMode: 'dependency'
+      chunksSortMode: 'dependency',
+      excludeChunks: ['app']
     }),
     // keep module.id stable when vendor modules does not change
     new webpack.HashedModuleIdsPlugin(),
